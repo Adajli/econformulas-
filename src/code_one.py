c3e46760f3942468afac7bb9fe74f3a3b36a5a81
Adajli <ajli@smith.edu>
2025-12-02 17:19:14 -0500

    Change code_one.py

--- src/code_one.py
+++ src/code_one.py
@@ -1,10 +1,12 @@
 class BudgetLinebasics:
-    def __init__(self, priceX,priceY):
+    def __init__(self, priceX,priceY,MU_X,MU_Y):
         self.priceX = priceX
         self.priceY = priceY
+        self.MU_X = MU_X
+        self.MU_Y = MU_Y
 
-    def checkBudget(self, priceX,priceY,Income,X,Y):
-        if(priceX*X+priceY*Y <= Income):
+    def checkBudget(self, priceX,priceY,Income,MU_X,MU_Y):
+        if(priceX*MU_X+priceY*MU_Y <= Income):
             print("You can spend the item with this income.")
         else:
             print("You can not spend the money on your income.")
