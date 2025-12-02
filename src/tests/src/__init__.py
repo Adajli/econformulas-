17069d065647b6c10c6293e94883a5cfa228524d
Adajli <ajli@smith.edu>
2025-12-02 14:43:03 -0500

    File change

--- src/tests/src/__init__.py
+++ src/tests/src/__init__.py
@@ -2,3 +2,9 @@ class BudgetLinebasics:
     def __init__(self, priceX,priceY):
         self.priceX = priceX
         self.priceY = priceY
+
+    def checkBudget(self, priceX,priceY,Income,X,Y):
+        if(priceX*X+priceY*Y <= Income):
+            print("You can spend the item with this income.")
+        else:
+            print("You can not spend the money on your income.")
