48733d8d3f802992a5c9fcba8fd058cdf3e181b3
AkinyiOc <zochieng@smith.edu>
2025-12-02 20:20:01 -0500

    Add checks to the case

--- __opportunity_cost.py
+++ __opportunity_cost.py
@@ -3,17 +3,25 @@ class OpportunityCost:
     def __init__(self, time_value=None, default_units="dollars"):
     # time_value : value of one hour of time (float)
     # default_units : label for the cost units (str) 
+        if not isinstance(time_value, (float)):
+            raise ValueError("time_A must be a number")
         self.time_value = time_value
         self.default_units = default_units
     def cost_of_time(self,hours):
-    # Calculates opportunity cost of hours spent
+    # Calculates opportunity cost of hours 
+        if not isinstance(hours, (int, float)):
+            raise ValueError("hours must be a number")
+       
         self.cost = np.array(hours) * self.time_value
         return self.cost
     def compare_alternatives(self, value_A, value_B):
     #Compares opportunity costs of two alternatives
     #value_A: the benefit/value you get from choosing A
     #value_B: the benefit/value you get from choosing B
-
+        if not isinstance(value_A, (int, float)):
+            raise ValueError("value_A must be a number")
+        if not isinstance(value_B, (int, float)):
+            raise ValueError("value_B must be a number")
     #Returns:
     #   choose_A_cost = what you give up (value of B)
     #   choose_B_cost = what you give up (value of A) 
@@ -24,10 +32,14 @@ class OpportunityCost:
     def comparative_advantage(self, time_A, time_B):
     #Compares comparative advantage of two values
     #Lower opportunity cost = comparative advantage
-        #calculates opportunity cost
+        if not isinstance(time_A, (int, float)):
+            raise ValueError("time_A must be a number")
+        if not isinstance(time_B, (int, float)):
+            raise ValueError("time_B must be a number")
+    #calculates opportunity cost
         self.OC_A = time_A/time_B
         self.OC_B = time_B/time_A 
-        #checks for comparative advantage
+    #checks for comparative advantage
         if self.OC_A < self.OC_B:
             return f"A has comparative advantage (OC={self.OC_A:.3f})"
         elif self.OC_B < self.OC_A:
@@ -36,7 +48,7 @@ class OpportunityCost:
             return "Neither has a comparative advantage (opportunity costs are equal)"
         
 opp_cost = OpportunityCost(45)
-opp_cost.cost_of_time(5)
+opp_cost.cost_of_time(five)
 print(opp_cost.cost)
 opp_cost.compare_alternatives(10,14)
 print(opp_cost.cost_of_A,opp_cost.cost_of_B)
