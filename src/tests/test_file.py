947ab06ecafd8930810c379ad3e156f71fb00742
Adajli <ajli@smith.edu>
2025-12-10 11:27:51 -0500

    Change file name

--- src/tests/test_file.py
+++
@@ -1,7 +0,0 @@
-from ..budget_line_basics import BudgetLinebasics
-
-def test_Income():
-    budget = BudgetLinebasics(20,20)
-    output = budget.calculateIncome(10,10)
-    assert output > 0
-test_Income()
